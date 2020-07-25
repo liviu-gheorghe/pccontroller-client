9f12934ff86fa512dd6a5528fb56309b3a3f7900
liviu-gheorghe <liviu.ionut64@yahoo.com>
2020-07-25 10:35:50 +0300

    Update Local Ip Address Regex

--- app/src/main/java/com/liviugheorghe/pcc_client/util/IpAddressValidator.java
+++ app/src/main/java/com/liviugheorghe/pcc_client/util/IpAddressValidator.java
@@ -7,14 +7,16 @@ public class IpAddressValidator {
     private static final String zeroTo255 = "([01]?[0-9]{1,2}|2[0-4][0-9]|25[0-5])";
     private static final String IP_REGEXP = zeroTo255 + "\\." + zeroTo255 + "\\." + zeroTo255 + "\\." + zeroTo255;
     private static final Pattern IP_PATTERN = Pattern.compile(IP_REGEXP);
-    private static final String LOCAL_IP_REGEXP = "(^127\\.0\\.0\\.1)|(^10\\.)|(^172\\.1[6-9]\\.)|(^172\\.2[0-9]\\.)|(^172\\.3[0-1]\\.)|(^192\\.168\\.)";
+    private static final String LOCAL_IP_REGEXP = "(^127\\.0\\.0\\.1)|(^10.*)|(^172.1[6-9].*)|(^172.2[0-9].*)|(^172.3[0-1].*)|(^192.168.*)";
     private static final Pattern LOCAL_IP_PATTERN = Pattern.compile(LOCAL_IP_REGEXP);
 
     private static boolean isIpAddress(String input) {
+        if (input == null) return false;
         return IP_PATTERN.matcher(input).matches();
     }
 
     public static boolean isLocalIpAddress(String input) {
+        if (input == null) return false;
         return isIpAddress(input) && LOCAL_IP_PATTERN.matcher(input).matches();
     }
 }
