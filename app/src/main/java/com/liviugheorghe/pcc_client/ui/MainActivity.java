ca2bb94878c727b713d515742d97ea38a53111ba
liviu-gheorghe <liviu.ionut64@yahoo.com>
2020-08-07 20:17:28 +0300

    Updated MainActivity
    Updated MainActivity to set up the drawer layout adding click listeners for several options in the menu
    
    Updated MainActivity to bind the Client service in order to be able to close all the foreground services if the user
    clicks the "Close app" option located in the Appbar menu

--- app/src/main/java/com/liviugheorghe/pcc_client/ui/MainActivity.java
+++ app/src/main/java/com/liviugheorghe/pcc_client/ui/MainActivity.java
@@ -1,36 +1,51 @@
 package com.liviugheorghe.pcc_client.ui;
 
 import android.app.Activity;
+import android.content.ComponentName;
 import android.content.Intent;
+import android.content.ServiceConnection;
 import android.os.Bundle;
+import android.os.IBinder;
 import android.text.Editable;
 import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
+import com.google.android.material.navigation.NavigationView;
 import com.liviugheorghe.pcc_client.App;
 import com.liviugheorghe.pcc_client.R;
 import com.liviugheorghe.pcc_client.backend.Client;
 import com.liviugheorghe.pcc_client.util.IpAddressValidator;
 
-public class MainActivity extends AppCompatActivity {
-
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
     private final int GET_QR_CODE_INFORMATION = 3;
+    DrawerLayout drawer;
+    private Client client;
+    private ServiceConnection serviceConnection;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        drawer = findViewById(R.id.drawer);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar, R.string.open_drawer_content_description, R.string.close_drawer_content_description);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+        NavigationView navigationView = findViewById(R.id.navigation_view);
+        navigationView.setNavigationItemSelectedListener(this);
     }
 
 
@@ -38,6 +53,15 @@ public class MainActivity extends AppCompatActivity {
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.activity_main_appbar_menu, menu);
         return true;
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        try {
+            bindClientService();
+        } catch (Exception ignored) {
+        }
     }
 
     @Override
@@ -50,11 +74,24 @@ public class MainActivity extends AppCompatActivity {
                         Client.class
                 ).putExtra(App.EXTRA_TARGET_IP_ADDRESS, data.getStringExtra(App.EXTRA_TARGET_IP_ADDRESS));
                 startService(serviceIntent);
+                bindClientService();
             } else {
                 Toast.makeText(this, R.string.invalid_ip_address_toast_text, Toast.LENGTH_SHORT).show();
             }
         }
     }
+
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        try {
+            unbindService(serviceConnection);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
 
     public void connectButtonClick(View v) {
         if (App.CONNECTION_ALIVE) {
@@ -76,7 +113,30 @@ public class MainActivity extends AppCompatActivity {
                     Client.class
             ).putExtra(App.EXTRA_TARGET_IP_ADDRESS, targetIpAddress);
             startService(serviceIntent);
+            bindClientService();
         }
+    }
+
+
+    private void bindClientService() {
+        serviceConnection = new ServiceConnection() {
+            @Override
+            public void onServiceConnected(ComponentName name, IBinder service) {
+                Client.ClientBinder clientBinder = (Client.ClientBinder) service;
+                client = clientBinder.getClient();
+            }
+
+            @Override
+            public void onServiceDisconnected(ComponentName name) {
+            }
+        };
+
+        bindService(new Intent(this, Client.class), serviceConnection, 0);
+    }
+
+    public void closeAppMenuItemClick(MenuItem item) {
+        if (client != null) client.stopClient();
+        finish();
     }
 
     public void startScanButtonClick(View v) {
@@ -85,5 +145,38 @@ public class MainActivity extends AppCompatActivity {
         } else {
             startActivityForResult(new Intent(getApplicationContext(), QrCodeScannerActivity.class), GET_QR_CODE_INFORMATION);
         }
+    }
+
+    private boolean checkIfDeviceIsConnectedBeforeOpeningActivity() {
+        if (!App.CONNECTION_ALIVE) {
+            drawer.closeDrawer(GravityCompat.START);
+            Toast.makeText(this, R.string.device_not_connected_text, Toast.LENGTH_SHORT).show();
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.drawer_action_remote_control:
+                if (checkIfDeviceIsConnectedBeforeOpeningActivity())
+                    startActivity(new Intent(this, MainControlInterfaceActivity.class));
+                break;
+            case R.id.drawer_action_file_sharing:
+                if (checkIfDeviceIsConnectedBeforeOpeningActivity())
+                    startActivity(new Intent(this, FileSharingActivity.class));
+                break;
+            case R.id.drawer_action_link_sharing:
+                if (checkIfDeviceIsConnectedBeforeOpeningActivity())
+                    startActivity(new Intent(this, LinkSharingActivity.class));
+                break;
+            case R.id.drawer_action_touchpad:
+                if (checkIfDeviceIsConnectedBeforeOpeningActivity())
+                    startActivity(new Intent(this, TouchpadActivity.class));
+            default:
+                return false;
+        }
+        return true;
     }
 }
