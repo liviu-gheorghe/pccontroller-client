757ea532b630b10a2c4701d6b6247385164caa47
liviu-gheorghe <liviu.ionut64@yahoo.com>
2020-08-15 15:27:39 +0300

    Updated tests for MainActivity

--- app/src/androidTest/java/com/liviugheorghe/pcc_client/MainActivityTest.java
+++ app/src/androidTest/java/com/liviugheorghe/pcc_client/MainActivityTest.java
@@ -10,6 +10,7 @@ import org.junit.Rule;
 import org.junit.Test;
 
 import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.Espresso.openActionBarOverflowOrOptionsMenu;
 import static androidx.test.espresso.action.ViewActions.click;
 import static androidx.test.espresso.action.ViewActions.typeText;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
@@ -17,8 +18,11 @@ import static androidx.test.espresso.intent.Intents.intended;
 import static androidx.test.espresso.intent.matcher.IntentMatchers.hasComponent;
 import static androidx.test.espresso.matcher.RootMatchers.withDecorView;
 import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static androidx.test.espresso.matcher.ViewMatchers.withClassName;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static androidx.test.espresso.matcher.ViewMatchers.withText;
+import static androidx.test.platform.app.InstrumentationRegistry.getInstrumentation;
+import static org.hamcrest.Matchers.endsWith;
 import static org.hamcrest.Matchers.not;
 
 public class MainActivityTest {
@@ -29,24 +33,30 @@ public class MainActivityTest {
     public ActivityTestRule<MainActivity> rule = new ActivityTestRule<>(MainActivity.class);
 
     @Test
-    public void userCanEnterIpAddress() {
+    public void userCanEnterIpAddress() throws InterruptedException {
         String ipAddress = "192.168.1.120";
-        onView(withId(R.id.computer_ip_text_input)).perform(typeText(ipAddress));
-        onView(withId(R.id.computer_ip_text_input)).check(matches(withText(ipAddress)));
+        openActionBarOverflowOrOptionsMenu(getInstrumentation().getTargetContext());
+        onView(withText(R.string.action_connect_using_ip)).perform(click());
+        onView(withClassName(endsWith("EditText"))).perform(typeText(ipAddress));
+        onView(withClassName(endsWith("EditText"))).check(matches(withText(ipAddress)));
     }
 
     @Test
     public void whenUserEntersInvalidIpAddress_toastWithWarningMessageIsShown() throws InterruptedException {
         String invalidIpAddress = "100.123.12.34";
-        onView(withId(R.id.computer_ip_text_input)).perform(typeText(invalidIpAddress));
-        onView(withId(R.id.connect_button)).perform(click());
+        openActionBarOverflowOrOptionsMenu(getInstrumentation().getTargetContext());
+        onView(withText(R.string.action_connect_using_ip)).perform(click());
+        onView(withClassName(endsWith("EditText"))).perform(typeText(invalidIpAddress));
+        onView(withText("OK")).perform(click());
         onView(withText(R.string.invalid_ip_address_toast_text)).inRoot(withDecorView(not(rule.getActivity().getWindow().getDecorView()))).check(matches(isDisplayed()));
         Thread.sleep(LONG_DELAY);
     }
 
     @Test
     public void whenUserLeavesIpAddressBlank_toastWithWarningMessageIsShown() throws InterruptedException {
-        onView(withId(R.id.connect_button)).perform(click());
+        openActionBarOverflowOrOptionsMenu(getInstrumentation().getTargetContext());
+        onView(withText(R.string.action_connect_using_ip)).perform(click());
+        onView(withText("OK")).perform(click());
         onView(withText(R.string.invalid_ip_address_toast_text)).inRoot(withDecorView(not(rule.getActivity().getWindow().getDecorView()))).check(matches(isDisplayed()));
         Thread.sleep(LONG_DELAY);
     }
@@ -55,7 +65,9 @@ public class MainActivityTest {
     @Test
     public void whenUserTriesToClickConnectButtonWhileAnotherConnectionIsEstablished_toastWithWarningMessageIsShown() throws InterruptedException {
         App.CONNECTION_ALIVE = true;
-        onView(withId(R.id.connect_button)).perform(click());
+        openActionBarOverflowOrOptionsMenu(getInstrumentation().getTargetContext());
+        onView(withText(R.string.action_connect_using_ip)).perform(click());
+        onView(withText("OK")).perform(click());
         onView(withText(R.string.connection_already_established_toast_text)).inRoot(withDecorView(not(rule.getActivity().getWindow().getDecorView()))).check(matches(isDisplayed()));
         App.CONNECTION_ALIVE = false;
         Thread.sleep(LONG_DELAY);
